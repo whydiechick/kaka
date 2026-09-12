614ab8df3417f555b0f75499e328c032ef6bf237
whydiechick <alexenokevseev@yandex.ru>
2026-09-13 00:15:57 +0300

    паци просто соло могнул этого батечька

--- батечек твой.py
+++ батечек твой.py
@@ -1,3 +1,7 @@
-def bibo(a, b):
-    print(len(a + b))
-bibo("Hello", "World")
+def twoSum(self, array, target):
+    slovarik = {}
+    for i in range(len(array)):
+        difference = target - array[i]    
+        if difference in slovarik:
+            return [slovarik[difference], i]
+        slovarik[array[i]] = i
